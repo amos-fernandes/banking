6c99efcbafab9503a6c09e1bc32ecdb40d2cf1f0
Amós Fernandes <consultor.amos@hotmail.com>
2025-04-14 11:48:54 -0300

    change ingest id sentry

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
         </h1>
 
         <p className="description">
-          Click the button below, and view the sample error on the Sentry <a target="_blank" href="https://aibank-r0.sentry.io/issues/?project=4509117794156544">Issues Page</a>.
+          Click the button below, and view the sample error on the Sentry <a target="_blank" href="https://aibank.sentry.io/issues/?project=4509152085606400">Issues Page</a>.
           For more details about setting up Sentry, <a target="_blank" href="https://docs.sentry.io/platforms/javascript/guides/nextjs/">read our docs</a>.
         </p>
 
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3797d234515cd24ad7d6aac1de44ae78@o4509117787602944.ingest.us.sentry.io/4509117794156544",
+  dsn: "https://d8067ad4a3406215f0826c5c61350bb7@o4508596882112512.ingest.us.sentry.io/4509152085606400",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
