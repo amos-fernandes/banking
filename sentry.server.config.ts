568d734d9a09fe155d485a51ebdc059966478882
amos-fernandes <36972593+amos-fernandes@users.noreply.github.com>
2025-04-15 10:40:30 -0300

    Update sentry.server.config.ts
    fix sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://d8067ad4a3406215f0826c5c61350bb7@o4508596882112512.ingest.us.sentry.io/4509152085606400",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
